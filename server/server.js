9e3f0a700c39199213c786c4b7931ebcbdaedb62
Rodgers Angala <angalarodgers@gmail.com>
2023-03-12 23:01:57 +0300

    cors

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ import eventRoutes from "./routes/events.js";
 //middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.use(express.json());
