6c4dce311b0a72da3d6b33b47e76ce2bcd8444f0
Rodgers Angala <angalarodgers@gmail.com>
2023-03-12 11:46:23 +0300

    save connect

--- server/connect.js
+++ server/connect.js
@@ -1,9 +1,20 @@
 import mysql from "mysql";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "proswim_db",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.ROOT_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.error("Error connecting to database: " + err.stack);
+    return;
+  }
+  console.log("Connected to database as id " + db.threadId);
 });
