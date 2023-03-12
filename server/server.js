220b58273d42851402226f9fa5d7b0644f11ff32
Rodgers Angala <angalarodgers@gmail.com>
2023-03-12 19:13:50 +0300

    removed .env files

--- server/server.js
+++ server/server.js
@@ -23,6 +23,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      process.env.CLIENT_URL,
+      process.env.ADMIN_URL,
     ],
   })
 );
