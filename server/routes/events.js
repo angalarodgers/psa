2d386d43c048a1011efb2ab92522595f92d4d606
Rodgers Angala <angalarodgers@gmail.com>
2023-03-15 08:59:49 +0300

    Add

--- server/routes/events.js
+++ server/routes/events.js
@@ -11,6 +11,7 @@ import {
 const router = express.Router();
 
 router.post("/addEvent", saveEvent);
+router.post("/saveCalendarEvent", saveCalendarEvent);
 router.get("/getEvents", getEvents);
 router.get("/getSessions", getSessions);
 router.get("/getEventTime/:tm", getEventTime);
