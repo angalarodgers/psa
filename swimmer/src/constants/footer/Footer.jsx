be064eb32d431d80ac08c6bb3ffbc061f4dffd01
Rodgers Angala <angalarodgers@gmail.com>
2023-03-16 14:15:24 +0300

    update

--- swimmer/src/constants/footer/Footer.jsx
+++ swimmer/src/constants/footer/Footer.jsx
@@ -10,42 +10,6 @@ const footer = () => {
               © Pro Swim Academy Kenya
             </div>
           </div>
-          <div className="col-lg-6">
-            <ul className="nav nav-footer justify-content-center justify-content-lg-end">
-              <li className="nav-item">
-                <a href="#" className="nav-link text-muted" target="_blank">
-                  Customer
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="https://www.creative-tim.com/presentation"
-                  className="nav-link text-muted"
-                  target="_blank"
-                >
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="https://www.creative-tim.com/blog"
-                  className="nav-link text-muted"
-                  target="_blank"
-                >
-                  Swimming Events
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  href="https://www.creative-tim.com/license"
-                  className="nav-link pe-0 text-muted"
-                  target="_blank"
-                >
-                  Services
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     </footer>
