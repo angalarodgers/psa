2b788b147e7cf507e02e0f627dcf0e0b23b7225d
Rodgers Angala <angalarodgers@gmail.com>
2023-03-25 21:40:00 +0300

    swimmer

--- admin/src/pages/trainers/Swimmer.jsx
+++ admin/src/pages/trainers/Swimmer.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeRequest } from "../../axios";
 import ViewUser from "./ViewUser";
 import "react-confirm-alert/src/react-confirm-alert.css";
 import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "react-query";
 
 const Swimmer = ({ swimmer }) => {
+  const [trainer_events, setTrainerEvents] = useState([]);
+  const { isLoading, error, data } = useQuery(
+    `ordersAll${swimmer.username}`,
+    () =>
+      makeRequest.get("/events/getEvents").then((res) => {
+        setTrainerEvents(res.data);
+        console.log(res.data);
+        return res.data;
+      })
+  );
+
+  const filtered = trainer_events.filter(
+    (event) => event.trainer === swimmer.username
+  );
+
+  const filtered_today = trainer_events.filter((event) => {
+    return (
+      (!swimmer.username || event.trainer === swimmer.username) &&
+      event.date === new Date().toISOString().split("T")[0]
+    );
+  });
+
+  const today = new Date().toISOString().slice(0, 10);
+  const futureEvents = trainer_events.filter((event) => {
+    if (event.trainer !== swimmer.username) {
+      return false;
+    }
+    if (event.date > today) {
+      return true;
+    } else if (
+      event.date === today &&
+      event.endTime >
+        new Date().toLocaleTimeString("en-US", { timeZone: "America/New_York" })
+    ) {
+      return true;
+    }
+    return false;
+  });
+
   function confirmAlert(options) {
     const { title, message, buttons } = options;
     const confirmed = window.confirm(`${title}\n\n${message}`);
@@ -64,8 +104,33 @@ const Swimmer = ({ swimmer }) => {
                 <p className="mb-1 d-md-none d-block text-sm font-weight-bold text-darker">
                   {swimmer.username}
                 </p>
-                <p className="mb-0 text-xs  text-warning text-gradient  mb-4">
+                <p className="mb-0 text-xs  text-dark   mb-4">
                   <a href={`mailto:${swimmer.email}`}>{swimmer.email}</a>
+                </p>
+                <p className="mb-0 text-xs text-darker  mb-4">
+                  <span style={{ color: "green" }}>
+                    Todays Classes Assigned:{" "}
+                    <strong className="text-sm font-weight-bold">
+                      {filtered_today.length}
+                    </strong>
+                  </span>
+                </p>
+                <p className="mb-0 text-xs text-darker  mb-4">
+                  <span>
+                    Future Classes Assigned:{" "}
+                    <strong className="text-sm font-weight-bold">
+                      {futureEvents.length}
+                    </strong>
+                  </span>
+                </p>
+                <p className="mb-0 text-xs text-darker  mb-4">
+                  <span style={{ color: "orange" }}>
+                    Classes Done:{" "}
+                    <strong className="text-sm font-weight-bold">
+                      {" "}
+                      {filtered.length - futureEvents.length}
+                    </strong>
+                  </span>
                 </p>
 
                 <ViewUser swimmer={swimmer} key={swimmer.id} />
